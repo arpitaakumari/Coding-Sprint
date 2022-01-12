bf8cc226e1d0b55be5dbaabad04e52fa4dc79338
Arpita Kumari <appu.kumari25@gmail.com>
2022-01-12 20:17:27 +0530

    first commit

---
+++ Sorting/Bubble_Sort.cpp
@@ -0,0 +1,36 @@
+//Author :- [Arpita_Kumari]
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define hs ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long int ll;
+
+void fileioe()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+void solve()
+{
+    
+}
+
+int main() 
+{
+    hs;
+    fileioe();
+
+    ll t;
+    //t=1;
+    cin>>t;
+    for (int i=0; i<t; i++)
+    {
+        solve();
+    }
+    return 0;
+}
