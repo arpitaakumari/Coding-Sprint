a69b4219c5ac7829d439c5a5d417503786d9c297
Arpita Kumari <appu.kumari25@gmail.com>
2022-01-18 12:40:32 +0530

    commit

---
+++ Arrays/count_platforms.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int solve(int arr[], int dep[], int n)
+{
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n], dep[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[n];
+    for(int i=0; i<n; i++)
+        cin>>dep[n];
+    solve(arr, dep, n);
+    return 0;
+}
